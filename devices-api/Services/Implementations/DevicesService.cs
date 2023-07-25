0e7a99183cf64faaf9cc149e398b4d9e8ce64749
Parso Erik <branseb@gmail.com>
2023-07-25 13:20:07 +0200

    add test user

--- devices-api/Services/Implementations/DevicesService.cs
+++ devices-api/Services/Implementations/DevicesService.cs
@@ -4,6 +4,8 @@ public class DevicesService : IDevicesService
     private List<(string, string, RoleStatus)> _assignedDevices = new List<(string, string, RoleStatus)>() {
         ("branseb@gmail.com", "000001" ,RoleStatus.Admin),
         ("branseb@gmail.com", "000003",RoleStatus.Guest),
+            ("parsoerik@gmail.com", "000001" ,RoleStatus.Admin),
+        ("parsoeri@gmail.com", "000003",RoleStatus.Guest),
     };
 
     private List<(string id, string pin)> _pairingDevices = new List<(string, string)>();
